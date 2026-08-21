61e5fcd31606cc4e3f66f3037a703c0a2c27da15
Pivetzen <pivetzen@gmail.com>
2026-08-21 11:01:32 -0300

    Modify CSV URLs to include CORS proxy
    Updated CSV URLs to use corsproxy.io for fetch access.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// ==========================================
-// CONFIGURAÇÃO DOS LINKS PUBLICADOS (CSV)
-// ==========================================
-const CSV_JOGOS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXCCvBBp25kBOPTSlKPXBa5hNoOfPlwcOT8t8GXhwpfDMZj-nNm177BGpqJP-SBx_dhDaDIdntNxFO/pub?gid=0&single=true&output=csv';
-const CSV_GOLS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRXCCvBBp25kBOPTSlKPXBa5hNoOfPlwcOT8t8GXhwpfDMZj-nNm177BGpqJP-SBx_dhDaDIdntNxFO/pub?gid=648851691&single=true&output=csv';
+// Adicionado corsproxy.io/ para liberar o acesso via Fetch no navegador
+const CSV_JOGOS_URL = 'https://corsproxy.io/?' + encodeURIComponent('https://docs.google.com/spreadsheets/d/e/2PACX-1vRXCCvBBp25kBOPTSlKPXBa5hNoOfPlwcOT8t8GXhwpfDMZj-nNm177BGpqJP-SBx_dhDaDIdntNxFO/pub?gid=0&single=true&output=csv');
+const CSV_GOLS_URL = 'https://corsproxy.io/?' + encodeURIComponent('https://docs.google.com/spreadsheets/d/e/2PACX-1vRXCCvBBp25kBOPTSlKPXBa5hNoOfPlwcOT8t8GXhwpfDMZj-nNm177BGpqJP-SBx_dhDaDIdntNxFO/pub?gid=648851691&single=true&output=csv');
 
 // Mapeamento dos Times nos Grupos A e B
 const TEAMS = {
